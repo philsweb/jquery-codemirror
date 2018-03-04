d69c1533dacd786ea19b0348ffc11b0d248a2bd7
Philipp <philipp.nikolajev@suomenpt.fi>
2018-03-04 14:55:24 +0200

    Namespacing added

--- philsweb.jquery.codemirror.js
+++ philsweb.jquery.codemirror.js
@@ -1,33 +1,37 @@
 (function ($) {
-    $.fn.codemirror = function () {
+    $.fn.codemirrorInit = function () {
 
         $(this).init.prototype = $.extend({}, $(this).init.prototype, {
 
-            setValue: function (value) {
-                for (var i = 0; i < this.length; i++) {
-                    $.data(this[i], 'codemirror').setValue(value);
-                }
-            },
-            getValue: function () {
-                if (this.length > 1) {
-                    throw new Error("Only one DOM element can be selected");
-                }
-                return $.data(this[0], 'codemirror').getValue();
-            },
-            setOption: function (key, value) {
-                for (var i = 0; i < this.length; i++) {
-                    $.data(this[i], 'codemirror').setOption(key, value);
-                }
-            },
-            setOptions: function (optionsObject) {
-                for (var i = 0; i < this.length; i++) {
-                    var $this = this[i];
-                    $.each(optionsObject, function (key, value) {
-                        $.data($this, 'codemirror').setOption(key, value);
-                    });
-                }
+            codemirror: function(){
+              var $this = this;
+              return {
+                  setValue: function (value) {
+                      for (var i = 0; i < $this.length; i++) {
+                          $.data($this[i], 'codemirror').setValue(value);
+                      }
+                  },
+                  getValue: function () {
+                      if (this.length > 1) {
+                          throw new Error("Only one DOM element can be selected");
+                      }
+                      return $.data($this[0], 'codemirror').getValue();
+                  },
+                  setOption: function (key, value) {
+                      for (var i = 0; i < $this.length; i++) {
+                          $.data($this[i], 'codemirror').setOption(key, value);
+                      }
+                  },
+                  setOptions: function (optionsObject) {
+                      for (var i = 0; i < this.length; i++) {
+                          var element = $this[i];
+                          $.each(optionsObject, function (key, value) {
+                              $.data(element, 'codemirror').setOption(key, value);
+                          });
+                      }
+                  }
+              }
             }
-
         });
 
         return this.each(function () {
